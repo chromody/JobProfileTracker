829edf91a53da50d1da1f20c52ae5f9b4472ec00
Jesus Villanueva-Segovia <chromody@gmail.com>
2025-07-26 15:49:25 -0500

    got saving to local storage capabilities

--- background.js
+++ background.js
@@ -1,7 +1,28 @@
+class Profile {
+  constructor(firstName, lastName, name) {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.name = name
+  }
+}
+
+//this function is meant to save a profile locally to be loaded in later.
+function saveProfile() {
+  let profile = new Profile("Jesus", "Villanueva-Segovia", "Jesus Villanueva-Segovia")
+  let settingItem = browser.storage.local.set({ "Default": profile }).then(() => {console.log("Set {}", example.name)}, () => {console.log("Error setting profile")})
+}
+
+function loadProfile() {
+  console.log("Loading")
+  browser.storage.local.get("")
+}
+
 browser.runtime.onMessage.addListener((message) => {
   if (message.action === "save") {
-    browser.alarms.create({ delayInMinutes: 0.01667 });
-    console.log("Alarm created from content script.");
+    saveProfile()
   }
-});
+  if (message.action === "load") {
+    loadProfile()
+  }
+})
 
--- ui/popup/scripts.js
+++ ui/popup/scripts.js
@@ -1,3 +1,8 @@
 document.getElementById("savebutton").addEventListener("click", () => {
   browser.runtime.sendMessage({ action: "save" });
 })
+
+
+document.getElementById("loadbutton").addEventListener("click", () => {
+  browser.runtime.sendMessage({ action: "load" });
+})
